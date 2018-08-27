b0a134c6389202675c02b40dec56927294f1b4e0
MurKat <mknee123@gmail.com>
2018-08-27 15:37:01 -0400

    Add google fonts CDN and started gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+Required Files
+*/
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css');
+
+/**Development Server **/
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    proxy: 'http://localhost',
+    server: {
+      baseDir: "./*html"
+    }
+  });
+});
+
+/***
+TASKS
+**/
+
+/**
+ * The default gulp task
+ */
+gulp.task('default', function() {
+    console.log("working!")
+});
+
+
+/**
+ * Minify and combine CSS files, including Reset
+*/
