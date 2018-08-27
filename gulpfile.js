17ba9b08f5c17aef26af7031365c476f6eeaaf35
MurKat <mknee123@gmail.com>
2018-08-27 16:26:55 -0400

    Finished gulpfile tasks

--- gulpfile.js
+++ gulpfile.js
@@ -27,12 +27,74 @@ TASKS
 
 /**
  * The default gulp task
- */
+
 gulp.task('default', function() {
     console.log("working!")
 });
+ */
 
+ /**
+  * Minify and combine CSS files, including Reset
+ */
+ gulp.task('css-reset', function() {
+     gulp.src([
+             'dev/css/reset.css'
+         ])
+         .pipe(minifyCSS())
+         .pipe(concat('reset.css'))
+         .pipe(gulp.dest('assets/css'));
+ });
+ gulp.task('css', function() {
+     gulp.src([
+             'dev/css/stylesheet.css'
+         ])
+         .pipe(minifyCSS())
+         .pipe(concat('stylesheet.css'))
+         .pipe(gulp.dest('assets/css'));
+ });
 
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('dev/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('dev/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('assets/js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'));
+});
 /**
- * Minify and combine CSS files, including Reset
-*/
+ * Copy images from source to distributable
+ *
+ * This could be extended to create different
+ * quality versions of images, or an image sprite
+ */
+gulp.task('images', function() {
+    gulp.src([
+            'dev/img/**/*'
+        ])
+        .pipe(gulp.dest('assets/img'));
+});
+
+
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('dev/js/*.js', ['lint', 'scripts']);
+    gulp.watch('dev/css/*.css', ['css-reset', 'css']);
+    gulp.watch('dev/img/*', ['images']);
+    gulp.watch("*.html").on("change", reload);
+    browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+    });
+});
+
+// Default Task
+gulp.task('default', ['css-reset', 'css', 'images', 'lint', 'scripts', 'watch']);
